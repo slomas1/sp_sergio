9d4272681f3f5cb01d6ef102d48e7df32fbb3f46
Sergio Lomas <serg@Sergios-MacBook-Pro.local>
2019-04-16 18:27:22 -0700

    fix tcp examples

--- capstone/tcp-client.c
+++ capstone/tcp-client.c
@@ -1,26 +1,14 @@
-//Sergio Lomas
-//Group27
 #include        <sys/socket.h>  /* for socket() and bind() */
 #include        <stdio.h>               /* printf() and fprintf() */
 #include        <stdlib.h>              /* for atoi() and exit() */
 #include        <arpa/inet.h>   /* for sockaddr_in and inet_ntoa() */
 #include        <sys/types.h>
 #include        <string.h>
-#include        <strings.h>
 #include        <unistd.h>
-#include        <time.h>
-#include        <unistd.h>
-#include		"message.h"
 
 #define ECHOMAX 255             /* Longest string to echo */
 #define BACKLOG 128
 
-int bingoCard[5][5];
-int markCard[5][5];
-
-
-
-int squareNumber();
 void
 DieWithError(const char *errorMessage) /* External error handling function */
 {
@@ -28,267 +16,30 @@ DieWithError(const char *errorMessage) /* External error handling function */
         exit(1);
 }
 
-void str_cli( int sockfd)
+void
+str_cli(FILE *fp, int sockfd)
 {
 	ssize_t n;
-    char    sendline[ECHOMAX], recvline[ECHOMAX];
-	struct player_query qmsg;
-	struct game_query gqmsg;
-	struct game_Init gamemsg;
-	struct message msg;
-	char cmd;
+        char    sendline[ECHOMAX], recvline[ECHOMAX];
 
-       //while (fgets(sendline, ECHOMAX, fp) != NULL) 
-       		printf("\nType help to get a list of commands\n");
-	   	for(;;)
-       	{
-       		printf("\nCommand: ");
-       		scanf("%s",msg.command);
-			//sendline[ n ] = '\0';
-       		//printf("comparison = %d\n",strncmp(sendline,recvline,9));
-       		if(strncmp(msg.command,"register",8)==0)
-       		{
-       			printf("Player Name: ");
-       			scanf("%s",msg.arg1);
-       			printf("IP Address: ");
-       			scanf("%s",msg.arg2);
-       			printf("Port: ");
-       			scanf("%s",msg.arg3);
-            	write(sockfd, &msg, sizeof(struct message));
+        while (fgets(sendline, ECHOMAX, fp) != NULL) {
 
-            	if ( (n = read(sockfd, recvline, ECHOMAX)) == 0)
-            		DieWithError("str_cli: server terminated prematurely");
-               
-                printf("\nResponse from server:%s\n",recvline);
-       		}
-       		else if(strncmp(msg.command,"queryplayers",12)==0)
-       		{
-            	write(sockfd, &msg, sizeof(struct message));
+                write(sockfd, sendline, strlen(sendline));
 
-            	if ( (n = read(sockfd, &qmsg,sizeof(struct player_query) )) == 0)
-            		DieWithError("str_cli: server terminated prematurely"); 
-                printf("\nResponse from server:\nNumber of players:%d\nPlayers:\n%s\n",qmsg.players,qmsg.List);
-       		}
-       		else if(strncmp(msg.command,"startgame",9)==0)
-       		{
-       			printf("K Players: ");
-       			scanf("%s",msg.arg1);
-       			write(sockfd, &msg, sizeof(struct message));
-       			if ( (n = read(sockfd, &gamemsg,sizeof(struct game_Init) )) == 0)
-            		DieWithError("str_cli: server terminated prematurely"); 
-                printf("\nGame Identifier from server:%s\nPlayers:\n%s\n",gamemsg.response,gamemsg.participantList);
+                if ( (n = read(sockfd, recvline, ECHOMAX)) == 0)
+                        DieWithError("str_cli: server terminated prematurely");
 
-       		}
-       		else if(strncmp(msg.command,"querygames",10)==0)
-       		{
-            	write(sockfd, &msg, sizeof(struct message));
-            	if ( (n = read(sockfd, &gqmsg,sizeof(struct player_query) )) == 0)
-            		DieWithError("str_cli: server terminated prematurely"); 
-                printf("\nResponse from server:\nNumber of games:%d\nGames:\n%s\n",gqmsg.numGames,gqmsg.gameList);
-
-       		}
-       		else if(strcmp(msg.command,"end")==0)
-       		{
-       			printf("GameID: ");
-       			scanf("%s",msg.arg1);
-            	write(sockfd, &msg, sizeof(struct message));
-            	if ( (n = read(sockfd, recvline, ECHOMAX)) == 0)
-            		DieWithError("str_cli: server terminated prematurely");
-                printf("\nResponse from server:%s\n",recvline);
-
-       		}
-       		if(strncmp(msg.command,"deregister",10)==0)
-       		{
-       			printf("Name: ");
-       			scanf("%s",msg.arg1);
-            	write(sockfd, &msg, sizeof(struct message));       			
-
-       		}
-       		else if(strcmp(msg.command,"help")==0)
-       		{
-       			printf("\nHELP:Type any of the commands seen below\nregister\t- This will register a new player\nqueryplayers\t- This will query for players that have been registered\nstartgame\t- This will attempt to start a new game\nquerygames\t- This will query for the games currently being played\nexit\t\t- This will close the manager and client connected to it\n");
-       		}
-       		else if(strcmp(msg.command,"exit")==0)
-       		{
-       			write(sockfd, &msg, sizeof(struct message));
-       			exit(0);
-       		}
-
-        }	
-
+		recvline[ n ] = '\0';
+                fputs(recvline, stdout);
+        }
 }
 
-
-
-
-int BINGO()
-{
-		return 1;
-}
-void cardFill(char *calledNum)
-{
-	int callednumber=4;
-
-
-	for(int i=0;i<5;i++)
-	{
-		for(int j=0;j<5;j++)
-		{
-			if(callednumber==bingoCard[i][j])
-				markCard[i][j]=1;	
-		}
-	}
-}
-
-int winCheck()
-{
-	int win=0;
-	int b,i,n,g,o;
-	int i1,i2,i3,i4,i5;
-	b=0;i=0;n=0;g=0;o=0;
-	i1=0;i2=0;i3=0;i4=0;i5=0;
-
-	for(int i=0;i<5;i++)
-	{
-		for(int j=0;j<5;j++)
-		{
-			if(i==0 && markCard[4][j]==1)
-				i1++;
-			if(i==1 && markCard[4][j]==1)
-				i2++;
-			if(i==2 && markCard[4][j]==1)
-				i3++;
-			if(i==3 && markCard[4][j]==1)
-				i4++;
-			if(i==4 && markCard[4][j]==1)
-				i5++;
-
-			if(j==0 && markCard[i][0]==1)
-				b++;
-			if(j==1 && markCard[i][1]==1)
-				i++;
-			if(j==2 && markCard[i][2]==1)
-				n++;
-			if(j==3 && markCard[i][3]==1)
-				g++;
-			if(j==4 && markCard[i][4]==1)
-				o++;
-		}
-	}
-	if(i1>=5)	
-		win=1;
-	if(i2>=5)	
-		win=1;
-	if(i3>=5)	
-		win=1;
-	if(i4>=5)	
-		win=1;
-	if(i5>=5)	
-		win=1;
-	if(b>=5)	
-		win=1;
-	if(i>=5)	
-		win=1;
-	if(n>=5)	
-		win=1;
-	if(g>=5)	
-		win=1;
-	if(o>=5)	
-		win=1;
-	if(markCard[0][0]==1 && markCard[1][1]==1 && markCard[3][3]==1 && markCard[4][4]==1)
-		win=1;
-	if(markCard[4][0]==1 && markCard[3][1]==1 && markCard[1][3]==1 && markCard[0][4]==1)
-		win=1;
-
-	return win;
-}
-void cardGen()	
-{
-
-	int squareVal;
-	int flag=1;
-	
-	for(int i=0;i<5;i++)
-	{
-		for(int j=0;j<5;j++)
-		{
-
-			if(i==2 && j==2)
-			{
-				bingoCard[2][2]=0;
-			}
-			else
-			{
-				flag=1;
-				while(flag==1)
-				{
-					squareVal=squareNumber();
-					flag=0;
-					switch(j)
-					{
-						case 0:  
-						break;
-						case 1: squareVal+=15;
-						break;
-						case 2:  squareVal+=30;
-						break;
-						case 3: squareVal+=45;
-						break;
-						case 4: squareVal+=60;
-						break;
-					}
-					for(int k=0;k<5;k++)
-					{
-						for(int l=0;l<5;l++)
-						{
-						if(squareVal==bingoCard[k][l])
-							flag=1;
-							
-						}
-					}
-				}
-				bingoCard[i][j]=squareVal;
-			
-			}
-		}
-	}
-	for(int i=0;i<5;i++)
-	{
-		for(int j=0;j<5;j++)
-		{
-			printf("[%d]",bingoCard[i][j]);
-		}
-		printf("\n");
-	}
-
-//	printf("squareVal=%d\n",squareVal );
-}
-int squareNumber()
-{
-	int rando;
-    int flag=1;
-    srand(time(NULL)); // randomize seed
-    int i=0;
-    while (flag==1)
-    {   
-        rando=(rand() % 15);
-        flag=0; 
-    }
-
-    return rando+1;
-}
-void playerBingo()
-{
-	//	printf("bingo=%d\n", BINGO());
-
-	
-}
 int
 main(int argc, char **argv)
 {
 	int sockfd;
 	struct sockaddr_in servaddr;
-	struct message in_msg;
+
 	if (argc != 3)
 		DieWithError( "usage: tcp-client <Server-IPaddress> <Server-Port>" );
 	
@@ -298,12 +49,10 @@ main(int argc, char **argv)
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(atoi(argv[2]));
 	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
-	connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));	
-	markCard[2][2]=1;
 
-	str_cli(sockfd);		
-//cardGen();
-//playerBingo();
-//callerBingo();
+	connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+	str_cli(stdin, sockfd);		/* do it all */
+
 	exit(0);
 }
--- capstone/tcp-server.c
+++ capstone/tcp-server.c
@@ -3,31 +3,11 @@
 #include	<stdlib.h>		/* for atoi() and exit() */
 #include	<arpa/inet.h>	/* for sockaddr_in and inet_ntoa() */
 #include	<sys/types.h>
-#include    <string.h>
-#include    <time.h>
+#include	<string.h>
 #include	<unistd.h>
-#include    "message.h"
-#include    "playerDBArray.h"
-#include    "gameDBArray.h"
+
 #define	ECHOMAX	255		/* Longest string to echo */
 #define BACKLOG	128
-
-
-
-int registerPlayer(char *playerName,char *ipAddres,char *port);
-void selectKplayers( int k,int callerindex);
-struct game_Init  game_start(int k,char *callerAddr,char *callerName);
-int getID();
-
-
-struct player playerDB[100];
-struct game gameDB[100];
-struct player tempPlayerDB[100];
-
-struct player_query pquery();
-struct game_query gquery();
-struct game_init gameInit();
-//char callerIP[16];
 
 void 
 DieWithError(const char *errorMessage) /* External error handling function */
@@ -36,311 +16,59 @@ DieWithError(const char *errorMessage) /* External error handling function */
 	exit(1);
 }
 
-
 void
-msgController(int sockfd,struct sockaddr_in Client)
+EchoString(int sockfd)
 {
     ssize_t n;
-    char line[ECHOMAX];
-    char *callerIP=inet_ntoa(Client.sin_addr);
-    char callersName[25];
+    char    line[ECHOMAX];
 
-    struct message readMsg;
-    struct player_query queryMsg;
-    struct game_query gqueryMsg;
-    struct game_Init gameMsg;
-
-    if ( (n = read(sockfd, &readMsg, sizeof(struct message))) == 0 )
-    {
-   	    return; /* connection closed by other end */
-    }       
-    readMsg.command[ n ] = '\0';            
-    printf("Command = %s\n",readMsg.command );
-
-	if(strcmp(readMsg.command,"register") == 0 )
-	{
-		if(registerPlayer(readMsg.arg1,readMsg.arg2,readMsg.arg3) == 0)
-			strcpy(line,"Success");
-		else
-			strcpy(line,"FAILURE: Player exists");
-        
-        write(sockfd, line, n );
-	}
-	if(strcmp(readMsg.command,"queryplayers") == 0 )
-	{
-		queryMsg=pquery();
-       	 write(sockfd, &queryMsg, sizeof(struct player_query) );
-	}
-    if(strcmp(readMsg.command,"startgame") == 0 )
-    {
-        int k=atoi(readMsg.arg1);
-        strcpy(callersName,caller_Name(playerDB,callerIP));
-        gameMsg=game_start(k,callerIP,callersName);
-        write(sockfd, &gameMsg, sizeof(struct game_Init) );
-    }
-    if(strcmp(readMsg.command,"querygames") == 0 )
-    {
-        gqueryMsg=gquery();
-        printf("gquerymsg numbers of games = %d\n",gqueryMsg.numGames);
-
-        write(sockfd, &gqueryMsg, sizeof(struct game_query) );
-    }
-    if(strcmp(readMsg.command,"end") == 0 )
-    {
-        if(gameDB_exists(gameDB,readMsg.arg1)==1)
-        {
-            gameDB_Delete(gameDB,readMsg.arg1);
-            strcpy(line,"Success");
-        }
-        else
-            strcpy(line,"FAILURE");
+    for ( ; ; ) {
+	    if ( (n = read(sockfd, line, ECHOMAX)) == 0 )
+   	    	return; /* connection closed by other end */
 
         write(sockfd, line, n );
     }
-
-    if(strcmp(readMsg.command,"deregister") == 0 )
-    {
-        if(playerSearch(playerDB,readMsg.arg1)==1) 
-        {
-            playerDB_Delete(playerDB,readMsg.arg1);
-            strcpy(line,"Success");
-        }
-        else
-            strcpy(line,"FAILURE");
-
-        write(sockfd, line, n );
-    }
-    if(strcmp(readMsg.command,"exit") == 0 )
-        exit(0);
-}
-
-
-
-struct player_query pquery()
-{
-    struct player_query newPQ;
-    int numPlayers=playerDB_GetSize(playerDB);
-    char pqList[200];
-
-    newPQ.players=numPlayers;
-    
-    if (numPlayers==0)
-        strcpy(newPQ.List,"EMPTY");
-    else
-        strcpy(newPQ.List,playerDB_List(playerDB));
-
-    return newPQ;
-}
-struct game_query gquery()
-{
-    struct game_query newGQ;
-    int num_games=gameDB_GetSize(gameDB);
-    char gqList[200];
-
-    newGQ.numGames=num_games;
-    if (num_games==0)
-    {
-        strcpy(newGQ.gameList,"EMPTY");
-    
-    }
-    else
-        strcpy(newGQ.gameList,gameDB_List(gameDB));
-
-    return newGQ;
-}
-
-int
-deregisterPlayer(char *playerName)
-{
-	playerDB_Delete(playerDB,playerName);
-	return 1;
-}
-
-
-int
-registerPlayer(char *playerName,char *ipAddres,char *port)
-{
-    struct player newPlayer;
-    strcpy(newPlayer.name,playerName);
-    strcpy(newPlayer.ip,ipAddres);
-    strcpy(newPlayer.port,port);
-
-    int found=0;
-    found=playerSearch(playerDB,playerName);
-    if(found!=1)
-    {
-        playerDB_Add(playerDB,newPlayer);
-    }
-        return found;
-}
-
-int game_End(char *gameIdentifier,char *callerAddr)
-{
-return 0;
-}
-
-struct game_Init  game_start(int k,char *callerAddr,char *callerName)
-{
-	struct game_Init gameInit;
-
-    int numPlayers=playerDB_GetSize(playerDB);
-    int callerFound=playerSearch(playerDB,callerName);
-    int callIndex=callerIndex(playerDB,callerAddr);
-
-    char newid[20];
-    sprintf(newid,"%d",getID());
-    struct game newGame;
-	if(numPlayers>=k+1 && callerFound==1 )
-	{
-		selectKplayers(k,callIndex);
-        strcpy(newGame.gamePList,playerDB_List(tempPlayerDB));
-        strcpy(newGame.gameID,newid);
-        newGame.caller=playerDB[callIndex];
-        gameDB_Add(gameDB,newGame);
-
-        strcpy(gameInit.response,newGame.gameID);
-        strcpy(gameInit.participantList,playerDB_List(tempPlayerDB));
-        tempDB_Delete(tempPlayerDB);
-	}
-    else
-    {
-        strcpy(gameInit.response,"FAILURE");
-        strcpy(gameInit.participantList,"EMPTY");
-
-    }
-    
-    return gameInit;
-}
-
-int getID()
-{
-    int rando;
-    int count=0;
-    int flag=1;
-    int totalGames=gameDB_GetSize(gameDB);
-    srand(time(NULL)); // randomize seed
-    int i=0;
-    while (flag==1)
-    {   
-        rando=(rand() % 100);
-        flag=0;
-        for(int i=0;i<totalGames;i++)
-        {
-            if(atoi(gameDB[i].gameID) == rando)
-                flag=1;
-        }   
-    }
-
-    return rando;
-
-}
-
-void selectKplayers( int k,int callerindex)
-{
-
-    //int callerindex=callerIndex(playerDB,callerAddr);
-	int rando;
-    int count=0;
-	int flag=0;
-	int used[k]  ;
-	int numPlayers=playerDB_GetSize(playerDB);
-	srand(time(NULL)); // randomize seed
-    int i=0;
-    while (i<k)
-	{
-        rando=(rand() % numPlayers);
-        //printf("rando=%d,count=%d\n", rando,count);
-        flag=0;
-        if (rando == callerindex)
-        {
-          //  printf("\nflag=1 not added\n");   
-            flag=1;
-        }
-        else if(i==0 )
-        {
-            used[i]=rando;
-            count++;
-            i++;
-        }
-        else
-        {
-		  for(int j=0;j<count;j++)
-		  {
-           //printf("j=%d\n",j);
-			if(rando==used[j] )
-            {
-              // printf("\nflag=1 not added\n");   
-				flag=1;
-            }
-		}
-        if(flag==0)
-        {
-         //   printf("\nflag=0\n");   
-	       used[i]=rando;
-            count++;
-            i++;
-        }
-    }
-	}
-    for(int i=0;i<k;i++)
-    {
-        //tempPlayerDB[i]=playerDB[used[i]];
-        playerDB_Add(tempPlayerDB,playerDB[used[i]]);
-
-    //printf("rand[%d]=%d\n",i, used[i]);
-    //printf("tempPlayerDB[%d]=%s\n",i, tempPlayerDB[i].name);
-    }
-	
 }
 
 int
 main(int argc, char **argv)
 {
-   strcpy(playerDB[0].name,"Tail");
-   strcpy(gameDB[0].gameID,"Tail");
-   strcpy(tempPlayerDB[0].name,"Tail");
+    int sock, connfd;                /* Socket */
+    struct sockaddr_in echoServAddr; /* Local address */
+    struct sockaddr_in echoClntAddr; /* Client address */
+    unsigned int cliAddrLen;         /* Length of incoming message */
+    char echoBuffer[ECHOMAX];        /* Buffer for echo string */
+    unsigned short echoServPort;     /* Server port */
+    int recvMsgSize;                 /* Size of received message */
 
-    int sock, connfd;                
-    struct sockaddr_in echoServAddr;
-    struct sockaddr_in echoClntAddr;
-    unsigned int cliAddrLen;        
-    char echoBuffer[ECHOMAX];       
-    unsigned short echoServPort;    
-    int recvMsgSize;                
-
-    if (argc != 2)         
+    if (argc != 2)         /* Test for correct number of parameters */
     {
         fprintf(stderr,"Usage: %s <TDP SERVER PORT>\n", argv[0]);
         exit(1);
     }
 
-    echoServPort = atoi(argv[1]); 
+    echoServPort = atoi(argv[1]);  /* First arg:  local port */
 
+    /* Create socket for sending/receiving datagrams */
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         DieWithError("server: socket() failed");
 
-    memset(&echoServAddr, 0, sizeof(echoServAddr));
-    echoServAddr.sin_family = AF_INET;             
-    echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    echoServAddr.sin_port = htons(echoServPort);     
+    /* Construct local address structure */
+    memset(&echoServAddr, 0, sizeof(echoServAddr));   /* Zero out structure */
+    echoServAddr.sin_family = AF_INET;                /* Internet address family */
+    echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
+    echoServAddr.sin_port = htons(echoServPort);      /* Local port */
 
+    /* Bind to the local address */
     if (bind(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
         DieWithError("server: bind() failed");
   
 	if (listen(sock, BACKLOG) < 0 )
 		DieWithError("server: listen() failed");
 
-
 	cliAddrLen = sizeof(echoClntAddr);
 	connfd = accept( sock, (struct sockaddr *) &echoClntAddr, &cliAddrLen );
 
-
-for(;;)
-{
-    msgController(connfd,echoClntAddr);
-
-}
+	EchoString(connfd);
 	close(connfd);
 }
-//*/
-
